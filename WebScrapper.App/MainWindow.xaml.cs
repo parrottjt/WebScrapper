f956a77c7c9c795601964dbd028a3749798ccadf
parrottjt <47727512+parrottjt@users.noreply.github.com>
2021-05-02 15:53:51 -0400

    Scrapping dnddbeyond.com/spells grabs all the spell accessors

--- WebScrapper.App/MainWindow.xaml.cs
+++ WebScrapper.App/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WebScrapper.HTML;
 
 namespace WebScrapper.App
 {
@@ -20,9 +22,33 @@ namespace WebScrapper.App
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly string url = "https://www.dndbeyond.com/spells";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WebPageDataClick(object sender, RoutedEventArgs e)
+        {
+            List<string> accessorNames = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int pageIndex = 1; pageIndex <= 26; pageIndex++)
+            {
+                HtmlString.Text = $"Reading Page {pageIndex} of 26";
+                var response = GatherHTML.CallUrl(url + $"?page={pageIndex}").Result;
+                var result = GatherHTML.ParseHtml(response, "div", "class", "info");
+                accessorNames.AddRange(result.Select(node => GatherHTML.GetAttributeName(node, "data-slug")));
+                foreach (var node in result)
+                {
+                    sb.AppendLine(GatherHTML.GetAttributeName(node, "data-slug"));
+                }
+            }
+
+            HtmlString.Text = sb.ToString();
+            GatherHTML.WriteNamesToFile(accessorNames);
+            File.Open("AccessorNames.csv", FileMode.Open);
         }
     }
 }
